79e50c67330d5f2bcd5dc5c853e53cf2228eccae
binniac <agencysocialmediagenz@gmail.com>
2022-03-25 08:43:40 +0700

    bbind click event cells

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
+import { TURN } from "./constants.js";
 import {
   getCellElementAtIdx,
   getCellElementList,
   getCurrentTurnElement,
   getGameStatusElement,
 } from "./selectors.js";
-
 /**
  * Global variables
  */
-let currentTurn = "cross";
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
+function toggleTurn() {
+  currentTurn = currentTurn === "circle" ? TURN.CROSS : TURN.CIRCLE;
+
+  // update turn on DOM element
+  const currentTurnElement = getCurrentTurnElement();
+  currentTurnElement.classList.remove(TURN.CROSS, TURN.CIRCLE);
+  currentTurnElement.classList.add(currentTurn);
+}
+function handleCellClick(cell, index) {
+  // check cell isClicked
+  const isClicked =
+    cell.classList.contains(TURN.CROSS) || cell.classList.contains(TURN.CIRCLE);
+  if (isClicked) return;
+  // set selector cell
+  cell.classList.add(currentTurn);
+
+  //toggle turn
+  toggleTurn();
+}
+function initCellElementList() {
+  const cellElementList = getCellElementList();
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener("click", () => handleCellClick(cell, index));
+  });
+}
+(() => {
+  // bind click event for all cells
+  initCellElementList();
+  // bind click event for button replay
+})();
 /**
  * TODOs
  *
